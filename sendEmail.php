8bd76de1eeb826ef68d7a8c8545778de188dd68a
Jessica Gilligan <jessicagilligan@Jessicas-MacBook-Pro.local>
2017-04-19 09:20:10 -0400

    modified php with smtp config

--- sendEmail.php
+++ sendEmail.php
@@ -1,11 +1,21 @@
 <? 
+require_once "Mail.php";
+
 if (isset($_POST['redirect']))
     {
         //set up message
         $send_to = $_POST['send_to'];
         $send_from = $_POST['send_from'];
         $subject = $_POST['subject'];
-        $redirect = $POST['redirect']
+        $redirect = $POST['redirect'];
+
+        $smtp = Mail::factory('smtp', array(
+            'host' => 'ssl://smtp.gmail.com',
+            'port' => '465',
+            'auth' => true,
+            'username' => 'admin@fishingtybee.com',
+            'password' => 'FishingTybee17'
+        ))
 
         //set up header
         $header = "From: " . $send_from . "\r\n";
@@ -19,7 +29,7 @@ if (isset($_POST['redirect']))
 
         //send the email
         //The '@' supresses errors
-        @mail($send_to, $subject, $message, $header);
+        $mail = smtp->send($send_to, $subject, $message, $header);
 
         header("Location: " . $redirect)
     }
